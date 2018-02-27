0623358a7f11c9d142461c1e9f2c1e46351b1eb4
Frank Congson <fcongson@gmail.com>
2018-02-28 11:11:28 +1300

    Add section header styling

--- src/components/SectionHeader.vue
+++ src/components/SectionHeader.vue
@@ -18,4 +18,35 @@ export default {
 };
 </script>
 
-<style scoped />;
+<style scoped>
+header p {
+  position: relative;
+  color: #999999;
+}
+p {
+  text-transform: uppercase;
+  font-size: 0.75rem;
+  font-weight: 300;
+  margin: 0 0 0.5rem 0;
+  padding: 0 0 1rem 0;
+  letter-spacing: 0.25rem;
+}
+p:after {
+  content: "";
+  position: absolute;
+  margin: auto;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  width: 50%;
+  height: 1px;
+  background-color: rgba(0, 0, 0, 0.125);
+}
+h2 {
+  color: #555;
+  font-size: 1.75rem;
+  font-weight: 300;
+  line-height: 1.5;
+  margin: 0 0 1rem 0;
+}
+</style>
