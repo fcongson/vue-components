11667540cd111e5f6c1491198734001df7a158d6
Frank Congson <fcongson@gmail.com>
2018-02-28 10:54:37 +1300

    Create section header component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <HelloWorld/>
+    <!-- <HelloWorld/> -->
+    <hr/>
+    <h1>Section Header</h1>
+    <p>Section header with customizable <code><b>title</b></code> and <code><b>description</b></code></p>
+    <h2>usage</h2>
+    <pre><code>&lt;SectionHeader description="Nam vel ante sit amet libero scelerisque facilisis eleifend vitae urna" title="Morbi maximus justo"/&gt;</code></pre>
+    <h2>preview</h2>
+    <div class="component-preview">
+    <SectionHeader description="Nam vel ante sit amet libero scelerisque facilisis eleifend vitae urna" title="Morbi maximus justo"/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+// import HelloWorld from "./components/HelloWorld";
+import SectionHeader from "./components/SectionHeader";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    // HelloWorld,
+    SectionHeader
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1,
+h2 {
+  font-weight: normal;
+}
+pre {
+  padding: 20px;
+  background: #eeeeee;
+}
+.component-preview {
+  border-style: solid;
+  border-width: 1px;
+  border-color: #eeeeee;
 }
 </style>
---
+++ src/components/SectionHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <header class="align-center">
+    <p>{{description}}</p>
+    <h2>{{title}}</h2>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "SectionHeader",
+  props: ["description", "title"],
+  data() {
+    return {
+      description: "Description",
+      title: "Title"
+    };
+  }
+};
+</script>
+
+<style scoped />;
